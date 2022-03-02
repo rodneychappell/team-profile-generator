5cc3614d0a101d313f55588b2007150be3ae470b
Rodney chappell <tookie@Rodneys-Mini.attlocal.net>
2022-03-02 00:46:18 -0500

    commiting test

---
+++ tests/Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require('../lib/Employee');
+const employee = new Employee('rchapp','123','rchapp@mail.com');
+
+test('test if we can get the values for employee object', () => {
+    expect(employee.name).toBe('rchapp');
+    expect(employee.id).toBe('123');
+    expect(employee.email).toBe('rchapp@mail.com');
+});
+
+test('test if we can get the name from getName() method', () => {
+    expect(employee.getName()).toBe('rchapp');
+});
+
+test('test if we can get the name from getId() method', () => {
+    expect(employee.getId()).toBe('123');
+});
+
+test('test if we can get the name from getEmail() method', () => {
+    expect(employee.getEmail()).toBe('rchapp@mail.com');
+});
+
+test('test if we can get the name from getRole() method', () => {
+    expect(employee.getRole()).toBe('Employee');
+});
