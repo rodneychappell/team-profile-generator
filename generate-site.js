53a440826c7028f899878d57020d849002295e76
Rodney chappell <tookie@Rodneys-Mini.attlocal.net>
2022-03-02 23:37:55 -0500

    commiting site

--- generate-site.js
+++ generate-site.js
@@ -17,4 +17,37 @@ const generateTeam = (team) => {
         </div>
     </div>`;
     }
+
+    const generateEngineer = engineer => {
+        return `
+        <div class="col-4 mt-4">
+            <div class="card h-100">
+                <div class="card-header">
+                    <h3>${engineer.name}</h3>
+                    <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+                </div>
+                <div class="card-body">
+                    <p class="id">ID: ${engineer.id}</p>
+                    <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                    <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+                </div>
+            </div>
+        </div>`;
+    }
+    const generateIntern = function (intern) {
+        return `
+        <div class="col-4 mt-4">
+            <div class="card h-100">
+                <div class="card-header">
+                    <h3>${intern.name}</h3>
+                    <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+                </div>
+                <div class="card-body">
+                    <p class="id">ID: ${intern.id}</p>
+                    <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                    <p class="school">School: ${intern.school}</p>
+                </div>
+        </div>
+    </div>`
+    };
 }
--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const inquirer = require('inquirer');
 const Manager = require('./pages/Manager');
 const Engineer = require('./pages/Engineer');
 const Intern = require('./pages/Intern');
-const generateSite = require('');
+const generateSite = require('./generate-site');
 const fs = require("fs");
+const path = require("path");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "index.html");
-const path = require("path");
-/// const Choices = require("inquirer/lib/objects/choices");
+
 const teamMembers = [];
 
 const promptManager = () => {
@@ -69,7 +69,7 @@ const promptManager = () => {
     .then((answer) => {
       console.log(answer);
       const manager = new Manager(
-        answers.name,
+        answer.name,
         answer.employeeId,
         answer.email,
         answer.officeNumber
